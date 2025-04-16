da72f71ecf4dd54be1730e72e5b057084e8a7075
AfeezTemitope <belloafeez28@gmail.com>
2025-04-16 23:22:36 +0100

    deplyoment

--- server/server.js
+++ server/server.js
@@ -20,14 +20,14 @@ app.use(cors())
 
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, '/client/dist')))
-
 app.use('/api', apiRoutes)
 
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
-});
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '/client/dist')))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+    });
+}
 app.listen(port, () => {
     console.log('server started sexily at http://localhost:' + port)
 })
