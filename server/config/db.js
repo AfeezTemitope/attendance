04bd37b176966c027a48a1994fabaa3339a03029
Tbelzbby <belloafeez28@gmail.com>
2025-04-27 00:28:40 +0100

    adjusted my vercel configurations

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose"
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGO_URI,{
-            authMechanism: "SCRAM-SHA-1"
-        })
+        await mongoose.connect(process.env.MONGO_URI)
         console.log('mongoDB connected successfully')
     } catch (err){
         console.error('mongoDB connection error: ', err.message)
