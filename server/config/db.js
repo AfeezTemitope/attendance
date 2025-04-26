9d029b9148f0e916934f99f47a1a3668996991bd
Tbelzbby <belloafeez28@gmail.com>
2025-04-26 22:45:50 +0100

    Bypass saslprep with SCRAM-SHA-1

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose"
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI,{
+            authMechanism: 'SCRAM-SHA-1',
+        })
         console.log('mongoDB connected successfully')
     } catch (err){
         console.error('mongoDB connection error: ', err.message)
